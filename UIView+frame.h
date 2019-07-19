06d40bf2a9fa495d6e6fad0d1194457e3bf6bdc8
YNET <ynet@YNETdeMacBook-Pro.local>
2019-07-19 15:33:27 +0800

    category

---
+++ UIView+frame.h
@@ -0,0 +1,17 @@
+//
+//  UIView+frame.h
+//  test20190717
+//
+//  Created by YNET on 2019/7/17.
+//  Copyright © 2019 YNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (frame)
+-(void)myPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIView+frame.m
@@ -0,0 +1,16 @@
+//
+//  UIView+frame.m
+//  test20190717
+//
+//  Created by YNET on 2019/7/17.
+//  Copyright © 2019 YNET. All rights reserved.
+//
+
+#import "UIView+frame.h"
+
+@implementation UIView (frame)
+-(void)myPrint
+{
+    NSLog(@"fsdfsaf");
+}
+@end
